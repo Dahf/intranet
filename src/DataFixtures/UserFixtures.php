2fb568df4f523733c5a107e3269fd4009a700a6c
Dahf <flacezockt@gmail.com>
2021-10-12 11:36:22 +0200

    bug fixed

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -17,13 +17,8 @@ class UserFixtures extends Fixture
         // $product = new Product();
         // $manager->persist($product);
         $user = new User();
-<<<<<<< HEAD
         $user->setEmail("123@gmail.com");
         $user->setUsername("Michael Maxwell");
-=======
-        $user->setEmail("1234@gmail.com");
-        $user->setUsername("Michael Maxwell2");
->>>>>>> 9ce9310b159d96fdebc80fba2e8429bdfef2d0fd
         $user->setRoles(["ROLE_USER"]);
         $user->setStatus("Online");
         $user->setPassword($this->passwordHasher->hashPassword(
